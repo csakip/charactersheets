38c34bbeccbf6fa7e47f4630b3977c1e97b5443a
Csokav <4134708+csakip@users.noreply.github.com>
2025-05-21 08:04:37 +0100

    Refactor: move charsheets to dir

--- src/charSheets/WoDuCharacterSheetPage.tsx
+++ src/charSheets/WoDuCharacterSheetPage.tsx
@@ -6,9 +6,9 @@ import { Toast } from "primereact/toast";
 import { Tooltip } from "primereact/tooltip";
 import { useEffect, useRef, useState } from "react";
 import { Fragment } from "react/jsx-runtime";
-import CharacterSheetBottom from "./components/CharacterSheetBottom";
-import { saveCharsheet } from "./supabase";
-import { abilities, attributes, Charsheet, skills, WoduData } from "./constants";
+import CharacterSheetBottom from "../components/CharacterSheetBottom";
+import { saveCharsheet } from "../supabase";
+import { abilities, attributes, Charsheet, skills, WoduData } from "../constants";
 
 export default function WoDuCharacterSheetPage({
   loadedCharsheet,
